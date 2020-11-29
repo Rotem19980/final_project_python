40f004be7146b6ab0f21364b43b3d89b296ab19c
Rotem <rotem19980@gmail.com>
2020-11-29 19:47:26 +0200

    fixed file url

--- functions.py
+++ functions.py
@@ -5,7 +5,7 @@ import os #this is needed for using directory paths and manipulating them
 import sys
 from pip._vendor.distlib.compat import raw_input
 
-class Employees_list:
+class Employee(object):
 
     def __init__(self, employee_id, name, phone, age):
         self.employee_id = employee_id
@@ -13,24 +13,17 @@ class Employees_list:
         self.phone = phone
         self.age = age
 
-    def add_employee(self):
+class Employees_list(object):
+    def __init__(self, list_of_employees):
+        self.list_of_employees = list_of_employees
+
+    def add_employee(self, new_employee):
         """
         This function gets the credentials of a new employee from user and adds it to the employees file.
         """
-        print("Please add credentials of a new employee (employee_id, name, phone, age): ")
-        self.employee_id = exceptions_tests.employee_id_test()
-        self.name = exceptions_tests.name()
-        self.phone = exceptions_tests.phone_test()
-        self.age = exceptions_tests.age_test()
-        new_employee = list()
-        new_employee.append(self.employee_id)
-        new_employee.append(self.name)
-        new_employee.append(self.phone)
-        new_employee.append(self.age)
-        with open("employees.csv", "a", newline="") as csvFile:
+        with open(self.list_of_employees, mode='a') as csvFile:
             fileout = csv.writer(csvFile, delimiter=',', quoting=csv.QUOTE_ALL)
-            fileout.writerow(new_employee)
-# Done
+            fileout.writerow([new_employee.employee_id, new_employee.name, new_employee.phone, new_employee.age])
 
     def add_employees_from_file(self):
         """
@@ -38,7 +31,7 @@ class Employees_list:
         only if all the data of all employees is supplied.
         new_employees = a string.
         """
-        new_employees_file_path = raw_input("Please add the file path of the employees you wish to add: ")
+        new_employees_file_path = raw_input("Please add the full file path of the employees you wish to add: ")
         try:
             with open(new_employees_file_path) as infile:
                 f = open(new_employees_file_path, 'r+')
@@ -49,6 +42,8 @@ class Employees_list:
                 f.close()
         except FileNotFoundError:
             return "File does not exist."
+
+
 
     #how to check if all data is supplied in the file
 
@@ -134,27 +129,34 @@ class Employees_list:
 
     # to do
 
-    def monthly_attendance_report(self):
-        """
-        The function prints the attendance data of all employees from the last month.
-        """
-        this_month = datetime.datetime.now().month
-        with open('attendance_log.csv', 'r') as csvfile:
-            content = csv.reader(csvfile, delimiter=',')
-            for row in content:
-                month = int(row[1].split('-')[1])
-                if month == this_month:
-                    return row
-    # Done
+def monthly_attendance_report():
+    """
+    The function prints the attendance data of all employees from the last month.
+    """
+    this_month = datetime.datetime.now().month
+    with open('attendance_log.csv', 'r') as csvfile:
+        content = csv.reader(csvfile, delimiter=',')
+        for row in content:
+            month = int(row[1].split('-')[1])
+            if month == this_month:
+                return row
+    Done
 
-    def late_employees_report(self):
-        """
-        The function prints the attendance data of all employees who were late (came after 09:30).
-        """
-        with open('attendance_log.csv', 'r') as csvfile:
-            content = csv.reader(csvfile, delimiter=',')
-            for row in content:
+def late_employees_report():
+    """
+    The function prints the attendance data of all employees who were late (came after 09:30).
+    """
+    with open(r'C:\Users\rotem\PycharmProjects\final_project_python\attendance_log.csv', 'r') as csvfile:
+        content = csv.reader(csvfile, delimiter=',')
+        for row in content:
+            if row:
                 hour = str(row[1].split(' ')[1])
                 if hour > "09:30:00":
-                    return row
-    #Done
+                    print(row)
+
+
+# def main():
+#
+#
+# if __name__ == '__main__':
+#     main()
