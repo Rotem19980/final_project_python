4048174019c23c261bc422c78989807893b065e8
Rotem <rotem19980@gmail.com>
2021-01-07 23:48:41 +0200

    added csv_data_is_supplied_test function to exceptions_tests.py.

--- exceptions_tests.py
+++ exceptions_tests.py
@@ -1,3 +1,17 @@
+import pandas as pd
+from pip._vendor.distlib.compat import raw_input
+
+def csv_data_is_supplied_test():
+   try:
+       csv_path_from_user = raw_input("Please insert the csv path: ")
+       test = pd.read_csv(csv_path_from_user)
+       x = test.isnull().any(axis=1)
+       missing_data_rows = x[x == True].index.to_numpy()
+       if missing_data_rows.size > 0:
+           print("There's some data missing, please check your csv.")
+   except FileNotFoundError:
+       print('File does not exist.')
+
 def employee_id_test():
     """
     This function gets from the user the employee's id as a string and checks if it's valid.
@@ -18,7 +32,7 @@ def employee_id_test():
         except ValueError:
             return 'Error! Please insert a valid employee id.'
 
-def name():
+def full_name_test():
     """
     This function gets from the user a name of an employee as a string and checks if it's valid.
     name = a string.
@@ -32,6 +46,8 @@ def name():
                 return 'Error! Please insert a valid employee name.'
         except ValueError:
             return 'Error! Please insert a valid employee name.'
+
+# to check that we get a full name
 
 def phone_test():
     """
@@ -67,6 +83,5 @@ def age_test():
                 return "Sorry, please insert a valid age."
         except ValueError:
             return 'Error! Please insert a valid employee age.'
-
 
 
--- functions.py
+++ functions.py
@@ -4,19 +4,24 @@ import exceptions_tests
 import os
 from pip._vendor.distlib.compat import raw_input
 
+EMPLOYEE_DOESNT_EXIST_MSG = "Sorry, the employee doesn't exist."
+EMPLOYEE_ALREADY_EXISTS_MSG = "Sorry, the employee is already exist."
+LATE_HOUR = "09:30:00"
+ADD_EMPLOYEES_FROM_FILE_MSG = "Please add the full file path of the employees you wish to add: "
+EMPLOYEE_ATTENDANCE_DOESNT_EXIST_MSG = "Sorry, the employee has no data in the attendance report."
 
 class Employee(object):
 
-    def __init__(self, employee_id, name, phone, age):
+    def __init__(self, employee_id, full_name, phone, age):
         """
         This function gets the employee's id, name, phone and age from user.
         employee_id = a string.
-        name = a string.
+        full_name = a string.
         phone = a string.
         age = a string.
         """
         self.employee_id = employee_id
-        self.name = name
+        self.full_name = full_name
         self.phone = phone
         self.age = age
 
@@ -31,14 +36,18 @@ class EmployeesList(object):
         self.list_of_employees = list_of_employees
 
     def add_employee(self, new_employee):
-        new_employee_data = [new_employee.employee_id, new_employee.name, new_employee.phone, new_employee.age]
-        with open(self.list_of_employees, 'r+') as file1:
-            existing_lines = csv.reader(file1)
-            for row in existing_lines:
-                if new_employee_data in row:
-                    print("Sorry, the employee is already exist.")
+        """
+        This function gets the credentials of a new employee and employees file path from from user and adds it to the file.
+        new_employee = new employee's credentials.
+        """
+        with open(self.list_of_employees, 'r+') as list_of_employees_file:
+            list_of_employees_data = csv.reader(list_of_employees_file)
+            for row in list_of_employees_data:
+                if new_employee.employee_id in row:
+                    print(EMPLOYEE_ALREADY_EXISTS_MSG)
                     return
-            file1.write(str(new_employee_data) + '\n')
+            list_of_employees_file.write(str(new_employee.employee_id) + ', ' + new_employee.name +
+                                         ', ' + str(new_employee.phone) + ', ' + str(new_employee.age) + '\r\n')
 
     def add_employees_from_file(self):
         """
@@ -46,7 +55,7 @@ class EmployeesList(object):
         only if all the data of all employees is supplied.
         new_employees_file_path = a string.
         """
-        new_employees_file_path = raw_input("Please add the full file path of the employees you wish to add: ")
+        new_employees_file_path = raw_input(ADD_EMPLOYEES_FROM_FILE_MSG)
         try:
             with open(new_employees_file_path) as infile:
                 f = open(new_employees_file_path, 'r+')
@@ -68,7 +77,7 @@ class EmployeesList(object):
         file and then deletes the old employees file. Afterwards, it saves it as the main employees file (employees).
         employees_name = a string.
         """
-        self.name = exceptions_tests.name()
+        self.full_name = exceptions_tests.full_name_test()
         with open("employees_edit.csv", "w") as my_empty_csv:
             pass
         with open('employees.csv', 'r') as inp, open('employees_edit.csv', 'w') as out:
@@ -100,62 +109,63 @@ class EmployeesList(object):
                 # open file in write mode
             with open("employees.csv", "w") as f:
                 for line in data_to_keep:
-                    if line in data_to_erase:
-                        pass
-                    else:
+                    if line not in data_to_erase:
                         f.write(line)
         except FileNotFoundError:
             return "File does not exist."
     # check if all data is supplied
 
-    def mark_attendance(self):
+    def mark_attendance(self, new_employee):
         """
         The function gets an employee's id as the input and saves the date and time of him in the attendance log file.
         employee_id = a 9 numbers integer.
         monthly_report_url = a string.
         """
-        monthly_report_url = input("Please enter monthly attendance url: ")
-        self.employee_id = exceptions_tests.employee_id_test()
-        csv_data = list()
-        csv_data.append(self.employee_id)
-        csv_data.append(datetime.datetime.now())
+        attendance_data = list()
+        attendance_data.append(new_employee.employee_id)
+        name_to_save = None
+        attendance_time = datetime.datetime.now()
+        attendance_time = attendance_time.strftime('%Y/%m/%d %H:%M:%S')
+        attendance_time = str(attendance_time)
+        attendance_data.append(attendance_time)
         with open(self.list_of_employees) as File:
             reader = csv.reader(File, delimiter=',', quotechar=',',
                                 quoting=csv.QUOTE_MINIMAL)
             for row in reader:
-                name = row[1]
-                if self.employee_id == row[0]:
-                    csv_data.append(name)
-        with open(monthly_report_url, "a") as csvFile:
-            fileout = csv.writer(csvFile, delimiter=',', quoting=csv.QUOTE_ALL)
-            fileout.writerow(csv_data)
+                if row:
+                    if str(new_employee.employee_id) == str(row[0]):
+                        name_to_save = row[1]
+                        attendance_data.append(str(name_to_save))
+            if name_to_save == None:
+                print(EMPLOYEE_DOESNT_EXIST_MSG)
+            log_file = r'C:\Users\rotem\PycharmProjects\final_project_python\attendance_log.csv'
+        with open(log_file, 'a') as csvFile:
+            log_file = csv.writer(csvFile, delimiter=',', quoting=csv.QUOTE_ALL)
+            log_file.writerow(attendance_data)
 
-    # Done
-
-    def attendance_report_of_employee(self):
-        """
-        The function gets an employee's id as the input and prints all the entries of his attendance.
-        id_input = a 9 numbers integer.
-        monthly_report_url = a string.
-        """
-        monthly_report_url = input("Please enter monthly attendance url: ")
-        self.employee_id = exceptions_tests.employee_id_test()
-        with open(monthly_report_url, 'r') as csvfile:
-            content = csv.reader(csvfile, delimiter=',')
-            for row in content:
-                if row[0] == self.employee_id:
-                    return row
-
-    # to do
+def attendance_report_of_employee():
+    """
+    The function gets an employee's id as the input and prints all the entries of his attendance.
+    employee_id = a 9 numbers integer.
+    """
+    employee_id = input("please insert the employee's id:")
+    employee_exists_in_attendance_log = False
+    with open(r'C:\Users\rotem\PycharmProjects\final_project_python\attendance_log.csv', 'r') as csvfile:
+        content = csv.reader(csvfile, delimiter=',')
+        for row in content:
+            if row:
+                if employee_id in row:
+                    print(row)
+                    employee_exists_in_attendance_log = True
+        if employee_exists_in_attendance_log is False:
+            print(EMPLOYEE_ATTENDANCE_DOESNT_EXIST_MSG)
 
 def monthly_attendance_report():
     """
     The function prints the attendance data of all employees from the last month.
-    monthly_report_url = a string.
     """
-    monthly_report_url = input("Please enter monthly attendance url: ")
     this_month = datetime.datetime.now().month
-    with open(monthly_report_url, 'r') as csvfile:
+    with open(r'C:\Users\rotem\PycharmProjects\final_project_python\attendance_log.csv', 'r') as csvfile:
         content = csv.reader(csvfile, delimiter=',')
         for row in content:
             if row and '-' in row[1]:
@@ -172,10 +182,8 @@ def late_employees_report():
         for row in content:
             if row:
                 hour = str(row[1].split(' ')[1])
-                if hour > "09:30:00":
+                if hour > LATE_HOUR:
                     print(row)
-
-
 # def main():
 #
 #
